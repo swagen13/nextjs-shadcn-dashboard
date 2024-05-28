ce4a1e2163e92e8ac3fa2e460a623d408dc8f291
swagen13 <106917556+swagen13@users.noreply.github.com>
2024-05-28 14:05:22 +0700

    dashboard middleware update

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,16 @@
-import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
-import { config } from "./pages/api/auth/[...nextauth]";
+import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth/next";
+import { config } from "@/pages/api/auth/[...nextauth]";
 
 export async function middleware(req: NextRequest) {
   // Get the session token
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-
-  console.log("req.nextUrl.pathname");
+  const session = await getServerSession(config);
 
   // Check if the user is trying to access the signin page
   if (req.nextUrl.pathname === "/signin") {
     // If the user is logged in, redirect them to the dashboard
-    if (token) {
+    if (session) {
       console.log("User is logged in, redirecting to /dashboard");
       return NextResponse.redirect(new URL("/", req.url));
     }
@@ -28,7 +26,7 @@ export async function middleware(req: NextRequest) {
     "/skills/:path*",
     "/subSkill",
     "/subSkill/:path*",
-    "/childrenSkill/",
+    "/childrenSkill",
     "/childrenSkill//:path*",
   ];
 
@@ -41,7 +39,7 @@ export async function middleware(req: NextRequest) {
   console.log("Is protected route:", isProtectedRoute);
 
   // If the user is trying to access a protected route and is not authenticated
-  if (isProtectedRoute && !token) {
+  if (isProtectedRoute && !session) {
     console.log("User is not logged in, redirecting to /signin");
     return NextResponse.redirect(new URL("/signin", req.url));
   }
